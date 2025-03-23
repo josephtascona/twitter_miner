cc2b1e2517c457c8d9fe8d71604432ab3c9a1558
Joseph Tascona <joseph.tascona@gmail.com>
2025-03-23 01:18:38 -0400

    Added git ignore for my API keys

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+from dotenv import load_dotenv
+import os
+
 import tweepy
 from tweepy import OAuthHandler
+
+load_dotenv()
  
-consumer_key = 'YOUR-CONSUMER-KEY'
-consumer_secret = 'YOUR-CONSUMER-SECRET'
-access_token = 'YOUR-ACCESS-TOKEN'
-access_secret = 'YOUR-ACCESS-SECRET'
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_secret = os.getenv("ACCESS_SECRET")
  
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
