1c42c7df6604f7c8a41bf644b605913419f7c1c2
Joseph Tascona <joseph.tascona@gmail.com>
2025-03-23 01:46:57 -0400

    Added ability to view 10 latest tweets from a user

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ import tweepy
 from tweepy import OAuthHandler
 
 load_dotenv()
- 
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_secret = os.getenv("ACCESS_SECRET")
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth)
+bearer_token = os.getenv("BEARER_TOKEN")
+
+client = tweepy.Client(bearer_token=bearer_token)
+
+response = client.get_users_tweets(id="1850280150249984000", max_results=10)
+for tweet in response.data:
+    print(tweet.text)
